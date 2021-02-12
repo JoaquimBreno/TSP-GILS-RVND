70c5661cac46e819c9cb0217106d037f34000f25
JoaquimBreno <winz993@gmail.com>
2021-02-12 19:11:24 -0300

    Swap Start - RVND Season

--- GILS-RVND-TSP/leitor-instancias/src/main.cpp
+++ GILS-RVND-TSP/leitor-instancias/src/main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 double **matrizAdj; // matriz de adjacencia
 int dimension;      // quantidade total de vertices
 
-void printData();
+
 
 // Estrutura com dados de arestas removidas e custos
 struct InsertionInfo {
@@ -19,11 +19,46 @@ struct InsertionInfo {
     double custo; // delta ao inserir o nó na aresta
 };
 
+// Swap
+double swap(vector <int> s){
+  double delta;
+
+
+  for( int i = 1; i < s.size(); i++){
+    for( int j = i + 1; j < s.size(); j++){
+      // Para i = 2 , j = 5 ...
+      // c24 +  c26 + c51 + c53 - ( c21 + c23 + c54 + c56)
+      // cij-1 + cij+1 + cji-1 + cji+1 - (cii-1 + cii+1 + cjj-1 + cjj+1)
+      delta= matrizAdj[s[i]][s[j-1]] + matrizAdj[s[i]][s[j+1]] + matrizAdj[s[j]][s[i-1]] + matrizAdj[s[j]][s[i+1]] - ( matrizAdj[s[i]][s[i-1]] + matrizAdj[s[i]][s[i+1]] + matrizAdj[s[j]][s[j-1]] + matrizAdj[s[j]][s[j+1]]);
+    }
+  }
+
+
+}
+
+// Estruturas de vizinhança
+void RVND(vector <int> &s){
+  swap(s);
+}
+
+// Calcula o custo total da distância entre todos os nós da solução
+double custoTotal(vector <int> s){
+
+  double custoTotal = 0;
+  for(int i = 0; i < s.size()-1; i++){
+    custoTotal += matrizAdj[s[i]][s[i+1]];
+  }
+
+  return custoTotal;
+}
+
+// Função vector que retorna uma solução viável para o tsp
 vector<int> construcao(double alfa, vector<int> listaDeCandidatos){
 
   vector<int> s;
 
   // Inicia o percurso com o depósito
+  s.push_back(listaDeCandidatos[0]);
   s.push_back(listaDeCandidatos[0]);
   listaDeCandidatos.erase(listaDeCandidatos.begin());
 
@@ -50,7 +85,7 @@ vector<int> construcao(double alfa, vector<int> listaDeCandidatos){
   
     // Ordenação SD pelos custos de inserção
     sort( custoInsercao.begin(), custoInsercao.end(), [](InsertionInfo a, InsertionInfo b){ 
-      return a.custo < b.custo;
+      return a.custo < b.custo; // Retorna o custo de inserção do menor ao maior
     });    
     // Gerador de desordem na definição dos elementos iniciais por meio de um valor aleatório
     int sAleatorios = alfa * custoInsercao.size(); 
@@ -69,37 +104,55 @@ vector<int> construcao(double alfa, vector<int> listaDeCandidatos){
   return s;
 }
 
+
+
 int main(int argc, char **argv){
 
   readData(argc, argv, &dimension, &matrizAdj);
-
+  int iIls;
   // Seed para geração de números aleatórios
   srand(time(NULL));
 
-  // Início do algoritmo principal
-  /** Geração de valor aleatório para a construção**/
-  double valorRand = (rand() % 100) /100.0 ;
-  /** Vetor que armazena as cidades **/
-  vector<int> cidades;
-  for (int i = 1; i << dimension; i++){ // gera vetor para as cidades
-    cidades.push_back(i);
+  // Controla o número de tentativas de melhorar a solução
+  if(dimension>=150){
+    iIls=dimension/2;
   }
- 
-  vector <int> so = construcao(valorRand, cidades);
+  else{
+    iIls=dimension;
+  }
 
-  for( int c = 0; c < so.size(); c++ ){
-   cout << so[c] << endl;
+  // Início do algoritmo principal
+
+  for( int iMax = 1; iMax <= 50; iMax++){
+  //srand(time(NULL));
+  // Vetor que armazena as cidades 
+  vector<int> cidades;
+  //double somatorioS;/**somatoriosA;**/
+  // Geração de valor aleatório alfa para a construção
+  double valorRand = (rand() % 100) /100.0 ;
+
+  // Produz vetor para as cidades
+    for (int i = 1; i << dimension; i++){ 
+      cidades.push_back(i);
+    }
+
+  vector <int> s = construcao(valorRand, cidades);
+  vector <int> sA = s; 
+
+  int iterIls = 0;
+  
+   while(iterIls < iIls){
+    
+   RVND(s);
+
+   double somatorioS = custoTotal(s);
+   // double somatoriosA = custoTotal(sA);
+    //if
+   // iterIls++;
+    cout << somatorioS << endl;
+    //}
   }
-  //printData();
+
   return 0;
 }
 
-void printData(){
-  cout << "dimension: " << dimension << endl;
-  for (size_t i = 1; i <= dimension; i++){
-    for (size_t j = 1; j <= dimension; j++){
-      cout << matrizAdj[i][j] << " ";
-    }
-    cout << endl;
-  }
-}
